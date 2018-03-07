715b432bb483a638e08a18611843efa493155086
lijiuyi <lijiuyi1995@outlook.com>
2018-03-07 18:39:02 +0800

    第四次提交

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blog import views
 urlpatterns = [
 
-    path('index/',views.IndexView.as_view()),
+    path('index/',views.IndexView.as_view(),name='index'),
     #path('test/',views.get_name),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import ListView
 from blog import models
-
+import time
 
 # Create your views here.
 
@@ -11,10 +11,18 @@ class IndexView(ListView):
     model = models.Blog
     template_name = 'base.html'
     context_object_name = 'blogs'
-    paginate_by = 1
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context =super(IndexView, self).get_context_data(**kwargs)
         context['tags'] = models.Tag.objects.all()
+        context['time'] = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        context['blog_category_learn'] = models.Blog.objects.filter(category__category__contains='技术随笔').count()
+        context['blog_category_life'] = models.Blog.objects.filter(category__category__contains='生活笔记').count()
+        archive_ = models.Blog.objects.datetimes('created_time','month',order='DESC',)
+        archive = []
+        for i in archive_:
+            archive.append(str(i)[:7])
+        context['archive']=archive
         return context
 
